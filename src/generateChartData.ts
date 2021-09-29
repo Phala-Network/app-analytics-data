44a8ef85e5294de5278fdc7a7a016ac44415aabf
limichange <limichange@hotmail.com>
2021-09-29 17:06:46 +0800

    feat: create chartData

--- src/generateChartData.ts
+++ src/generateChartData.ts
@@ -3,17 +3,19 @@ import path from 'path'
 import { writeJson } from './writeJson'
 
 const dailyFilsPath = path.resolve(__dirname, '../data/daily')
-
+const chartData: {}[] = []
 const result = fs.readdirSync(dailyFilsPath)
 
 result.map((file) => {
   const data = require(`${dailyFilsPath}/${file}`)
 
   console.log(data)
+
+  chartData.push(data)
 })
 
 console.log('result', result)
 
 const fileName = 'chartData.json'
 
-writeJson(`./data/chart/${fileName}`, {})
+writeJson(`./data/chart/${fileName}`, chartData)
