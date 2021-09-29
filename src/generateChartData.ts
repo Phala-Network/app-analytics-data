ce60046742fc33c26e3ce41d23c45d0fbd4681fe
limichange <limichange@hotmail.com>
2021-09-29 17:29:56 +0800

    feat: chart data

--- src/generateChartData.ts
+++ src/generateChartData.ts
@@ -6,12 +6,23 @@ const dailyFilsPath = path.resolve(__dirname, '../data/daily')
 const chartData: {}[] = []
 const result = fs.readdirSync(dailyFilsPath)
 
-result.map((file) => {
+result.map((file, index) => {
   const data = require(`${dailyFilsPath}/${file}`)
 
-  console.log(data)
+  if (index === 0) {
+    chartData.push({
+      ...data,
+      reward: 0,
+    })
+  } else {
+    const lastData = require(`${dailyFilsPath}/${result[index - 1]}`)
+    const lastDataReward = lastData ? lastData.reward : data.reward
 
-  chartData.push(data)
+    chartData.push({
+      ...data,
+      reward: data.reward - lastDataReward,
+    })
+  }
 })
 
 console.log('result', result)
